de469fcff05ae2f1dbf80cfa45150dbd8880f171
loris-catalano <161748350+loris-catalano@users.noreply.github.com>
2024-05-17 17:32:56 +0200

    Pom modification and added new module for Review microservice

---
+++ Review/src/test/java/it/unisannio/gruppo3/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisannio.gruppo3.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
