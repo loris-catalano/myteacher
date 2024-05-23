219c2910585f9c4e6502efc5d26ecfcd86d404d6
loris-catalano <161748350+loris-catalano@users.noreply.github.com>
2024-05-23 22:37:21 +0200

    Creation of LessonsAgenda microservice and implementation of CRUD operation

---
+++ LessonsAgenda/src/main/java/it/unisannio/gruppo3/lessonsagenda/persistence/LessonsAgendaDAO.java
@@ -0,0 +1,32 @@
+package it.unisannio.gruppo3.lessonsagenda.persistence;
+
+import it.unisannio.gruppo3.entities.LessonsAgenda;
+
+import java.util.ArrayList;
+
+public interface LessonsAgendaDAO {
+
+    String DATABASE_NAME = "myteacher";
+    String COLLECTION_LESSONS_AGENDAS = "LessonsAgendas";
+
+    String ELEMENT_ID = "id";
+    String ELEMENT_LESSONS = "lessons";
+
+    String ELEMENT_HIGHEST_ID = "highestId";
+
+    boolean dropDB();
+
+    boolean createDB();
+
+    Long createLessonsAgenda(LessonsAgenda lessonsAgenda);
+
+    LessonsAgenda getLessonsAgenda(Long id);
+
+    ArrayList<LessonsAgenda> getAllLessonsAgendas();
+
+    LessonsAgenda updateLessonsAgenda(LessonsAgenda lessonsAgenda);
+
+    boolean deleteLessonsAgenda(Long id);
+
+    boolean closeConnection();
+}
---
+++ LessonsAgenda/src/test/java/it/unisannio/gruppo3/lessonsagenda/LessonsAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisannio.gruppo3.lessonsagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonsAgendaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
