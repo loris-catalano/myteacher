a86efc78ebcf22f5cb180f59b231ad2dadca6277
loris-catalano <161748350+loris-catalano@users.noreply.github.com>
2024-06-04 20:29:34 +0200

    implementation of hashing

--- MyTeacherGateway/src/main/java/it/unisannio/gruppo3/myteachergateway/security/PasswordEncoderPlain.java
+++ MyTeacherGateway/src/main/java/it/unisannio/gruppo3/myteachergateway/security/PasswordEncoderPlain.java
@@ -2,14 +2,30 @@ package it.unisannio.gruppo3.myteachergateway.security;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class PasswordEncoderPlain implements PasswordEncoder {
     @Override
     public String encode(CharSequence charSequence) {
-        return charSequence.toString();
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hashBytes = digest.digest(charSequence.toString().getBytes(StandardCharsets.UTF_8));
+
+            return Base64.getEncoder().encodeToString(hashBytes);
+
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Hashing algorithm not found: " + e.getMessage());
+        }
+
+        return null;
     }
 
     @Override
     public boolean matches(CharSequence charSequence, String s) {
-        return charSequence.toString().equals(s);
+        return encode(charSequence).equals(s);
     }
 }
