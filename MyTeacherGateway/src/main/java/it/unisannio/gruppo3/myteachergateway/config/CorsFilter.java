6104bd327894bd9cdf22f9d936e4641cbd9e7dc4
loris-catalano <161748350+loris-catalano@users.noreply.github.com>
2024-06-08 18:13:19 +0200

    Fix Cors

--- MyTeacherGateway/src/main/java/it/unisannio/gruppo3/myteachergateway/config/CorsFilter.java
+++ MyTeacherGateway/src/main/java/it/unisannio/gruppo3/myteachergateway/config/CorsFilter.java
@@ -5,12 +5,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
-public class CorsFilter extends OncePerRequestFilter {
+public class CorsFilter extends org.springframework.web.filter.CorsFilter {
+
+    public CorsFilter(CorsConfigurationSource configSource) {
+        super(configSource);
+    }
 
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
