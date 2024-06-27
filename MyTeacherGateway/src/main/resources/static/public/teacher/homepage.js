735769a2198b36ea2804c9f4a85879fcce635be4
loris-catalano <161748350+loris-catalano@users.noreply.github.com>
2024-06-27 12:09:00 +0200

    frontend update

--- MyTeacherGateway/src/main/resources/static/public/teacher/homepage.js
+++ MyTeacherGateway/src/main/resources/static/public/teacher/homepage.js
@@ -27,7 +27,7 @@ function availableLessonHTML(lesson, teacher){
     `
 }
 
-function agendaLessonHTML(lesson, teacher){
+function agendaLessonHTML(lesson, student){
     let d = lesson.startLesson
 
     const date = d.split('T')[0]
@@ -35,8 +35,7 @@ function agendaLessonHTML(lesson, teacher){
 
     return `
     <div class="lesson-element">
-    <p><strong>Docente</strong>: ${teacher.firstName} ${teacher.lastName}</p>
-    <p><strong>Materia</strong>: ${lesson.subject}</p>
+    <p><strong>Studente</strong>: ${student.firstName} ${student.lastName}</p>
     <p><strong>Data</strong>: ${date} ${time}</p>
     <p><strong>Durata</strong>: ${lesson.duration} ore</p>
     <button type="submit" class="cancel-button" id="lesson-${lesson.id}" onclick="cancelLesson(id)">Disdici</button>
@@ -44,7 +43,7 @@ function agendaLessonHTML(lesson, teacher){
     `
 }
 
-function doneLessonHTML(lesson, teacher){
+function doneLessonHTML(lesson, student){
     let d = lesson.startLesson
 
     const date = d.split('T')[0]
@@ -52,11 +51,10 @@ function doneLessonHTML(lesson, teacher){
 
     return `
     <div class="lesson-element">
-    <p><strong>Docente</strong>: ${teacher.firstName} ${teacher.lastName}</p>
-    <p><strong>Materia</strong>: ${lesson.subject}</p>
-    <p><strong>Prezzo</strong>: € ${lesson.price}</p>
+    <p><strong>Studente</strong>: ${student.firstName} ${student.lastName}</p>
+    <p><strong>Costo</strong>: € ${lesson.price}</p>
     <p><strong>Data</strong>: ${date} ${time}</p>
-    <button type="submit" class="make-review-button" id="lesson-${lesson.id}" onclick="showReviewPopup(id)">Recensisci</button>
+    <!--<button type="submit" class="make-review-button" id="lesson-${lesson.id}" onclick="showReviewPopup(id)">Recensisci</button>-->
     </div>
     `
 }
@@ -116,7 +114,7 @@ function logout(){
 
 function fillLessonsAgenda(){
 
-    const lessonsAgendaId = localStorage.getItem("studentAgenda")
+    const lessonsAgendaId = localStorage.getItem("teacherAgenda")
     const lessonsAgenda = JSON.parse( getRequest(lessonsAgendaUrl+lessonsAgendaId).responseText )
 
     const lessonsIds = lessonsAgenda['lessons']
@@ -125,33 +123,15 @@ function fillLessonsAgenda(){
         currLessonId = lessonsIds[i]
 
         let lesson = JSON.parse( getRequest(lessonsUrl+currLessonId).responseText )
-        let teacher = JSON.parse(getRequest(teacherUrl + lesson.teacherId).responseText)
+        let student = JSON.parse(getRequest(studentUrl + lesson.studentId).responseText)
 
-        document.getElementById("agendaLessonsList").innerHTML += agendaLessonHTML(lesson, teacher);
+        document.getElementById("agendaLessonsList").innerHTML += agendaLessonHTML(lesson, student);
     }
 
 }
 
-function fillAvailableLessons(){
+function fillReviews(){
 
-    let xhrLessons = getRequest(lessonsUrl) //get all lessons
-
-    availableLessonsListDiv = document.getElementById("availableLessonsList")
-    availableLessonsListDiv.innerHTML = ''
-
-    if (xhrLessons.status === 200) {
-        let lessons = JSON.parse(xhrLessons.responseText)
-        for(i in lessons){
-            let lesson = lessons[i]
-            if (lesson.studentId != null) continue; //skip booked lessons
-
-            let teacher = JSON.parse(getRequest(teacherUrl + lesson.teacherId).responseText)
-
-            availableLessonsListDiv.innerHTML += availableLessonHTML(lessons[i], teacher);
-        }
-    }
-    else
-        return null;
 }
 
 function fillDoneLessons(){
@@ -231,55 +211,6 @@ function showReviewPopup(id){
     localStorage.setItem("currLessonId", id) // temporarly set the id of the lesson of the review
 }
 
-function makeReview(){
-    const lessonId = localStorage.getItem("currLessonId").split("-")[1]
-    localStorage.removeItem("currLessonId") // remove the temporary id of the lesson of the review
-    const lesson = JSON.parse(getRequest(lessonsUrl+lessonId).responseText)
-    const teacherId = lesson.teacherId
-    const studentId = Number(localStorage.getItem("id"))
-
-    const reviews = JSON.parse(getRequest(reviewUrl).responseText)
-    for(i in reviews){
-        review = reviews[i]
-
-        console.log(review)
-
-
-        if(review.studentId === studentId && review.teacherId === teacherId){
-            alert("Hai già fatto una recensione per questa lezione.")
-            window.location.pathname = "/public/student/homepage.html"
-            return
-        }
-    }
-
-    const stars = document.getElementById('stars').value;
-    const title = document.getElementById('title').value;
-    const description = document.getElementById('description').value;
-
-    console.log(lesson)
-    console.log(teacherId)
-
-    let body = {
-        stars: stars,
-        title: title,
-        body: description,
-        studentId: studentId,
-        teacherId: teacherId
-    };
-
-    body = JSON.stringify(body);
-
-
-    const req = postRequest(reviewUrl, body)
-
-    if(req.status === 201){
-        alert("Review submitted.")
-    }else{
-        alert("There was an error")
-    }
-
-}
-
 function seeTeacherReviews(id){
     id = id.split("-")[1]
 
@@ -303,46 +234,18 @@ function seeTeacherReviews(id){
     }
 }
 
-function search(){
-    let searchInput = document.getElementById("searchInput").value
-
-    let xhrLessons = getRequest(lessonsUrl) //get all lessons
-    if (xhrLessons.status === 200) {
-        let lessons = JSON.parse(xhrLessons.responseText)
-        document.getElementById("availableLessonsList").innerHTML = ""
-        for(i in lessons){
-            let lesson = lessons[i]
-
-            if (lesson.studentId != null || lesson.subject.toLowerCase() !== searchInput.toLowerCase()) continue; //skip booked lessons
-
-            let teacher = JSON.parse(getRequest(teacherUrl + lesson.teacherId).responseText)
-            console.log(teacher)
-            document.getElementById("availableLessonsList").innerHTML += availableLessonHTML(lessons[i], teacher);
-        }
-    }
-    else
-        return null;
-}
 
 
 document.getElementById("logout-href").addEventListener("click", logout)
 
-/*Check if search is empty so lessons available are resetted*/
-searchInput = document.getElementById("searchInput")
-searchInput.addEventListener("input", function checkIfEmpty() {
-    if (searchInput.value.trim() === '') {
-        fillAvailableLessons()
-    }
-})
 
 document.addEventListener('DOMContentLoaded', function() {
     topButton = document.getElementById("student-dropdown")
 
-    if(localStorage.getItem("Authorization") !== null) {
-        topButton.innerText = `🧑‍🎓 ${localStorage.getItem("firstName")} ${localStorage.getItem("lastName")}`
+    if(localStorage.getItem("Authorization") !== null && localStorage.getItem("Role") === "ROLE_TEACHER") {
+        topButton.innerText = `🧑‍🏫 ${localStorage.getItem("firstName")} ${localStorage.getItem("lastName")}`
 
         fillLessonsAgenda();
-        fillAvailableLessons();
         fillDoneLessons();
     }else{
         document.querySelector(".dropdown-content").style.display = 'none'
